b6667505df7ff2e553ea4042d6c86b56e06d9fc0
Ishimoto Shinobu <nagakamira@gmail.com>
2020-07-06 16:29:56 +0300

    terminal init
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- ke/include/terminal.h
+++ ke/include/terminal.h
@@ -1,1 +1,8 @@
 /*	$sunwave: terminal.h,v 1.0 2020/07/06 00:00:00 protonesso Exp $	*/
+
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+void terminal_initialize(void);
+
+#endif
--- ke/main.c
+++ ke/main.c
@@ -1,1 +1,8 @@
 /*	$sunwave: main.c,v 1.0 2020/07/06 00:00:00 protonesso Exp $	*/
+
+#include <terminal.h>
+
+void kmain(void) {
+	terminal_initialize();
+	for (;;) {}
+}
